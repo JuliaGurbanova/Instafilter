ab10905cd7a80a8055b903f26b6c12300ec99bbb
Julia <julia.gurbanova@gmail.com>
2023-10-04 15:46:01 +0300

    initial commit

---
+++ Project13_Instafilter/Project13_InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project13_InstafilterApp.swift
+//  Project13_Instafilter
+//
+//  Created by Julia Gurbanova on 11.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Project13_InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
